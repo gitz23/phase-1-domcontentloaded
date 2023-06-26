1bcd431b30ef362b70d97889e558b63fbd4aa9a3
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-26 11:44:02 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", eventFunction);
+
+function eventFunction(){
+    const paragraph = document.getElementById('text');
+    paragraph.textContent = 'This is really cool!' ;
+}
